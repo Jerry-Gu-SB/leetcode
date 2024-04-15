9bfce6558fe868e58c867014a7c27650b6171061
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-04-15 11:10:35 +0900

    started on LRU cache, think we making some good progress

--- 146. LRU Cache.py
+++ 146. LRU Cache.py
@@ -4,35 +4,73 @@
 # if a key is evicted, then remove from dictionary, and then remove from queue.
 # LRUCache initialization is just setting the global size variable and instantiating the dictionary
 # THIS IS SLOW BECAUSE REMOVE() RUNS IN LINEAR TIME
-from collections import defaultdict
+class Node:
+    def __init__(self, key=None, value=None, next=None, prev=None):
+        self.key = key
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
 
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.length = capacity
-        self.dict = defaultdict(int)
-        self.LRU = []
+        LL = LinkedList()
+        LL.head = Node()
+        LL.tail = Node()
+        LL.head.next = LL.tail
+        LL.tail.prev = LL.head
+
+        dicto = {}
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
-        if key in self.dict:
-            self.LRU.remove(key)
-            self.LRU.append(key)
-            return self.dict[key]
-        else:
-            return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self.LRU.remove(key)
-            self.LRU.append(key)
-            self.dict[key] = value
+        if key in self.dicto:
+
         else:
-            if len(self.LRU) == self.length:
-                del self.dict[self.LRU[0]]
-                del self.LRU[0]
-            self.dict[key] = value
-            self.LRU.append(key)
+            new_node = Node(key, value, LL.tail, LL.tail.prev)
+            if self.capacity == LL.length:
+
+            LL.tail.prev.next = new_node
+            LL.tail.prev = new_node
+
+        # from collections import defaultdict
+# class LRUCache:
+
+#     def __init__(self, capacity: int):
+#         self.length = capacity
+#         self.dict = defaultdict(int)
+#         self.LRU = []
+
+#     def get(self, key: int) -> int:
+#         if key in self.dict:
+#             self.LRU.remove(key)
+#             self.LRU.append(key)
+#             return self.dict[key]
+#         else:
+#             return -1
+
+
+#     def put(self, key: int, value: int) -> None:
+#         if key in self.dict:
+#             self.LRU.remove(key)
+#             self.LRU.append(key)
+#             self.dict[key] = value
+#         else:
+#             if len(self.LRU) == self.length:
+#                 del self.dict[self.LRU[0]]
+#                 del self.LRU[0]
+#             self.dict[key] = value
+#             self.LRU.append(key)
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
