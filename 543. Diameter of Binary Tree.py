5edcac883e4803a5518ebc3156e440ba0c807d85
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-04-19 18:30:27 +0900

    okay so where we got stuck is that only doing dfs through the binary tree isn't able to track the inner diameter, so in order to do that, you need to have a global variable to track that. or just have every node store it, and then max that, but that's a pain.

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -6,17 +6,13 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.max_diameter = 0
         def dfs(node):
             if node == None:
                 return 0
-            print("node value: " + str(node.val))
-            print(dfs(node.left), dfs(node.right))
-            return max(dfs(node.left) + 1, dfs(node.right) + 1)
-        if root.left and not root.right:
-            return dfs(root.left)
-        elif not root.left and root.right:
-            return dfs(root.right)
-        elif not root.right and not root.left:
-            return 0
-        else:
-            return dfs(root)
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.max_diameter = max(left + right, self.max_diameter)
+            return max(left, right) + 1
+        dfs(root)
+        return self.max_diameter
