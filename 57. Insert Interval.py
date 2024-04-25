f576814546cf73ae5cddf21b3dd1aa2c4b5ab0dd
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-04-25 12:29:58 +0900

    rewrote the code, only need to account for directly in between test case

--- 57. Insert Interval.py
+++ 57. Insert Interval.py
@@ -7,7 +7,6 @@ class Solution:
             #   1. other interval fully in between
             #   2. new interval goes over
             #   3. new interval goes under
-            print("overlap checking checked_interval: ", checked_interval, " new_interval: ", new_interval)
             if checked_interval[0] <= new_interval[0] <= checked_interval[1] or checked_interval[0] <= new_interval[
                 1] <= checked_interval[1] or (
                     checked_interval[0] >= new_interval[0] and checked_interval[1] <= new_interval[1]):
@@ -15,8 +14,32 @@ class Solution:
             else:
                 return False
 
-            # iterate through intervals O(n)
+        if newInterval[1] < intervals[0][0]:
+            intervals.insert(0, newInterval)
+            return intervals
+        elif newInterval[0] > intervals[-1][1]:
+            intervals.append(newInterval)
+            return intervals
 
+        solution = []
+        overlapped = False
+        temp_interval = [float('inf'), float('-inf')]
+        for i, cur_interval in enumerate(intervals):
+            if overlaps(cur_interval, newInterval):
+                temp_interval[0] = min(cur_interval[0], newInterval[0], temp_interval[0])
+                temp_interval[1] = max(cur_interval[1], newInterval[1], temp_interval[1])
+                overlapped = True
+                if (i == len(intervals) - 1):
+                    solution.append(temp_interval)
+            else:
+                if overlapped:
+                    solution.append(temp_interval)
+                    overlapped = False
+                solution.append(cur_interval)
+
+        return solution
+
+        # iterate through intervals O(n)
         # if not overlapping: append to new interval list
         # else:
         #   the first interval that you encounter, create a new temp interval (don't insert)
@@ -29,42 +52,56 @@ class Solution:
         # before/after can be checked manually at the start
         # between has to be checked every iteration: current iterator doesn't overlap, new_int[0] > cur_int[1]
 
-        in_between = False
-        subseq_interval = False
-        solution = []
-        temp_interval = [float("inf"), float("-inf")]
-        for cur_interval in intervals:
-            print(cur_interval)
-            if in_between:
-                print("IN_BETWEEN")
-                solution.append(cur_interval)
-                continue
+        # if len(intervals) == 1:
+        #     if overlaps(intervals[0], newInterval):
+        #         temp = intervals[0]
+        #         return [min(temp[0], newInterval[0]), max(temp[1], newInterval[1])]
+        #     else:
+        #         if newInterval[1] < intervals[0][0]:
+        #             intervals.insert(0, newInterval)
+        #             return intervals
+        #         elif newInterval[0] > intervals[-1][1]:
+        #             intervals.append(newInterval)
+        #             return invertals
 
-            if not overlaps(cur_interval, newInterval):
-                print("NOT OVERLAP")
-                # if subseq_interval and newInterval[0] > cur_interval[1]:
-                #     solution.append(temp_interval)
-                #     solution.append(cur_interval)
-                #     in_between = True
-                if subseq_interval:
-                    print("subsequent interval")
-                    solution.append(temp_interval)
-                    solution.append(cur_interval)
-                else:
-                    print("not sub interval")
-                    solution.append(cur_interval)
-            else:
+        # in_between = False
+        # subseq_interval = False
+        # solution = []
+        # temp_interval = [float("inf"), float("-inf")]
+        # for cur_interval in intervals:
+        #     print(cur_interval)
+        #     if in_between:
+        #         print("IN_BETWEEN")
+        #         solution.append(cur_interval)
+        #         continue
 
-                temp_interval[0] = min(cur_interval[0], newInterval[0], temp_interval[0])
-                temp_interval[1] = max(cur_interval[1], newInterval[1], temp_interval[1])
-                subseq_interval = True
-                print("should be just merging intervals: ", temp_interval)
+        #     if not overlaps(cur_interval, newInterval):
+        #         print("NOT OVERLAP")
+        #         # if subseq_interval and newInterval[0] > cur_interval[1]:
+        #         #     solution.append(temp_interval)
+        #         #     solution.append(cur_interval)
+        #         #     in_between = True
+        #         if subseq_interval:
+        #             print("subsequent interval")
+        #             solution.append(temp_interval)
+        #             solution.append(cur_interval)
+        #         else:
+        #             print("not sub interval")
+        #             solution.append(cur_interval)
+        #     else:
 
-        if not subseq_interval or not in_between:
-            if newInterval[1] < intervals[0][0]:
-                intervals.insert(0, newInterval)
-                return intervals
-            elif newInterval[0] > intervals[-1][1]:
-                intervals.append(newInterval)
-                return invertals
-        return solution
+        #         temp_interval[0] = min(cur_interval[0], newInterval[0], temp_interval[0])
+        #         temp_interval[1] = max(cur_interval[1], newInterval[1], temp_interval[1])
+        #         subseq_interval = True
+        #         print("should be just merging intervals: ", temp_interval)
+
+        # if not subseq_interval or not in_between:
+        #     if newInterval[1] < intervals[0][0]:
+        #         intervals.insert(0, newInterval)
+        #         return intervals
+        #     elif newInterval[0] > intervals[-1][1]:
+        #         intervals.append(newInterval)
+        #         return invertals
+
+        # if not solution: return temp_interval
+        # return solution
