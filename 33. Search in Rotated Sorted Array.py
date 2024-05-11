c27c9ac201f67228d604ca4470538e143c03756e
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-05-11 12:02:36 +0900

    yeah i'm having a lot of trouble wrappign my head around this. i think we'll give it one shot programming and if i can't get it or get any headway, we'll just look at the answers

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -7,3 +7,20 @@ class Solution:
         # note that the last digit is alwasy going to be the max of either the whole
         # thing or the pivited section. so you can check that to see if there's a pivot
         # in the first place, OR you can do your binary search inside there.
+
+        # you can find the pivot just by subtracting the first and second + 1?
+        # subtract to find the pivot, then run binary search on the right pivot point.
+        # actually it's not incrementing by 1 each time, so you can't get info from the ends
+
+        # maybe 2 binary searches for 2logn? 1 inary search to find the pivot, then
+        # inary search in the right pivot slot.
+
+        # or maybe just as simple as 1
+
+        if nums[0] < nums[-1]:
+            # binary search
+        else:
+            left = 0
+            right = len(nums)
+            mid = int((left + right)/2)
+            while left <= right:
