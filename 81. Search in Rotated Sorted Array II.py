12ecc5a658eb18b87fc8cdb33269241717c029a3
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-05-15 23:40:49 +0900

    I WAS RIGHT THE FIRST TIME. I ACTUALLY GASLIT MYSELF INTO NOT TRYING THE CORRECT ANSWER AAAAAAAAAAAAA WHYYYYYYYYYYYYY.

--- 81. Search in Rotated Sorted Array II.py
+++ 81. Search in Rotated Sorted Array II.py
@@ -15,21 +15,24 @@ class Solution:
 
             if nums[mid] == target:
                 return True
+
+            # i originially had these like this but i concinved myself that a while loop would do the same thing
+            # but IT DOESN'T.
             if nums[mid] == nums[left]:
-                left = mid + 1
+                left += 1
                 continue
             if nums[mid] == nums[right]:
-                right = mid - 1
+                right -= 1
                 continue
 
-            if nums[left] < nums[mid]:  # left sorted
-                if nums[left] < target < nums[mid]:
+            if nums[left] <= nums[mid]:  # left sorted
+                if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
 
             else:  # right side sorted
-                if nums[mid] < target < nums[right]:
+                if nums[mid] <= target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
