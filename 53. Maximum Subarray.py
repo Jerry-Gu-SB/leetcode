62be33d85f8cf45eeb1f6a27b5eabe60efa6d170
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-05-22 23:35:07 +0900

    yeah i wont lie dynamic programming is confusing af i gotta practice more of this.

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
         # kadane's algorithm. this was kind of what i was getting at before,
         # convince yourself this works
 
@@ -8,14 +7,25 @@ class Solution:
         # and the divide and conquer method. we're gonna be doing this problem
         # for a hot minute
 
-        # also you were trying to intuit the sliding window. you need to do more
-        # sliding window problems because you aren't recognizing that pattern.
-        if not nums: return []
+        # dynamic programming:
+        # find the work you have to do
+        # find a way to store the work youve already done
 
-        cur = 0
-        solution = -inf
-        for num in nums:
-            cur = max(num, cur + num)
-            solution = max(solution, cur)
+        # checking every subarray is just the smaller subarray plus an extra character
+        # so if you check 12345, when you check 1, 12, 123, 1234, 12345, you can recursively
+        # check 23, 234, 2345 as well and then memoize for next iteration
 
-        return solution
+        grid = [[] for i in len(nums)]
+        for i in range(len(nums)):
+
+    # also you were trying to intuit the sliding window. you need to do more
+    # sliding window problems because you aren't recognizing that pattern.
+    # if not nums: return []
+
+    # cur = 0
+    # solution = -inf
+    # for num in nums:
+    #     cur = max(num, cur + num)
+    #     solution = max(solution, cur)
+
+    # return solution
