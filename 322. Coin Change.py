e3e5283690f8c012be439b477926a0cc7b85d5ca
Jerry-Gu-SB <spiralbiscuit@gmail.com>
2024-07-05 16:32:55 -0400

    need to work with DP a bit more but this helped a lot remembering

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,35 +1,12 @@
-from collections import defaultdict
-
-
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0: return 0
-        if len(coins) == 1:
-            if coins[0] == amount:
-                return -1
-            else:
-                return 0
 
-        memo = defaultdict(int)
+        # try to do this using a recursive method next time too
 
-        def rec(cur_amount, coin_list):
-            global memo
-            if cur_amount in memo.keys():
-                return memo[cur_amount]
-            if cur_amount == coin[0]:
-                memo[cur_amount] = 1
-                return 1
-
-            cur_amount -= coin_list[-1]
-
-            if coin_list[-1] > cur_amount:
-                coin_list = coin_list[0:len(coin_list) - 1]
-
-            temp = rec(cur_amount, coin_list)
-            memo[cur_amount] = temp
-            return temp
-
-        return rec(coins, amount)
-
-
-
+        dp = [float('inf') for i in range(amount + 1)]
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - coin])
+        return dp[amount] if dp[amount] < float('inf') else -1
