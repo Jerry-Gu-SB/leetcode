aa84820a08a3cf152c95cf77cd29850e330b7ddb
Jerry Gu <ncq9fn@virginia.edu>
2024-07-16 22:30:44 -0400

    yeah this is kinda pissin me off i'm terrible at this, let's really try and learn this a bit better

--- 416. Partition Equal Subset Sum.py
+++ 416. Partition Equal Subset Sum.py
@@ -1,16 +1,22 @@
 class Solution:
+    from itertools import combinations
     def canPartition(self, nums: List[int]) -> bool:
         # brute force:
         # check every single subset, sum it against its opposite
         # NOT SUBSET, PERMUTATION
         # DID THE TREE, STARTING TO UNDERSTAND SOMETHING. LET ME COOK TOMORROW.
         half = sum(nums) / 2
-        print(sum(nums))
-        print(half)
-
-        for i in range(len(nums) + 1):
-            for j in range(i + 1, len(nums) + 1):
-                print(nums[i: j], sum(nums[i: j]))
-                if sum(nums[i: j]) == half:
+        memo = set()
+        for i in range(len(nums)):
+            combo = combinations(nums, i)
+            for comb in combo:
+                if sum(comb) == half:
                     return True
         return False
+
+        # we simply need to track the combinations but for the indices i think should be memoized
+        # you also don't need to keep going once you're past the sum.
+        # yeah i thinmk it's some form of like memo[0] + cur index, but you need to do it on a 2d scale maybe? man this shit is hard
+
+        # figure out the sub problems a bit more i guess.
+
