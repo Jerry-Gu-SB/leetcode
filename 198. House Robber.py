eef6c469251b20fa673545fc08635287faf91e53
Jerry Gu <ncq9fn@virginia.edu>
2024-07-29 22:00:12 -0400

    I GOT IT BABY. You wrote out the base case solutions, then you started seeing what happens if you added on numbers gradually. then you were able to figure out the trick of finding the max of the previous 2 solutions and adding the current number. let's fuckign go dude. good shit. good job. i'm actually proud of myself for figuring this one out.

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -10,3 +10,17 @@ class Solution:
         # if you break it down into a subproblem of length 4 then there are only 3 possiblities. if you break it down to length 3 and 2 there are only 2 possibilities.
 
         # yeah gonna have to think about it some more.
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums[0], nums[1])
+        if len(nums) == 3: return max(nums[0] + nums[2], nums[1])
+        dp = []
+        dp.append(nums[0])
+        dp.append(max(nums[0], nums[1]))
+        dp.append(max(nums[0] + nums[2], nums[1]))
+
+        for i in range(3, len(nums)):
+            dp.append(max(dp[i - 1], dp[i - 2] + nums[i]))
+        return dp[len(nums) - 1]
+
+
+
