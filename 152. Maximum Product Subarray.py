ec0f7c0028fdbb0a6527203379c09e504180d030
Jerry Gu <ncq9fn@virginia.edu>
2024-08-06 17:21:21 -0400

    yeah i feel like i'm getting closer. I made some progress so I don't want to abandon this one yet. but i'm getting kinda close. if i don't make significant progress next time we're giving up.

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -9,20 +9,21 @@ class Solution:
         # did not account for 0. i guess if it's zero then you just compare with the solution and if it's bigger at that the zero then you just make it the solution.
         if len(nums) == 1:
             return nums[0]
-        solution = float('-inf')
+        solution = nums[0]
         temp = 0
         even = True
+        cur_prod = nums[0]
 
         for i in range(len(nums)):
+            print(i, ". ", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution),
+                  "even: " + str(even))
             if i == 0:
-                cur_prod = nums[i]
+                if nums[i] < 0:
+                    even = False
                 continue
-            print(i, ". ", cur_prod, temp, solution, even)
-
+            cur_prod *= nums[i]
             if nums[i] == 0:
                 even = True
-            elif nums[i] > 0:
-                cur_prod *= nums[i]
             elif nums[i] < 0:
                 if not even:
                     temp = cur_prod
@@ -32,9 +33,12 @@ class Solution:
                     cur_prod *= temp
                     temp = 0
                     even = False
+
             solution = max(solution, cur_prod)
-            print(i, ". ", cur_prod, temp, solution, even)
-        print("last:", cur_prod, temp, solution, even)
+            print(i, ". ", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution),
+                  "even: " + str(even))
+        print("last--", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution),
+              "even: " + str(even))
         return max(solution, temp, cur_prod)
 
         # brute force: check every subarray
