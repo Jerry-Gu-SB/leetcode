7fea8c483c1a7687b784cf68459020a5c6873ff1
Jerry Gu <ncq9fn@virginia.edu>
2024-08-09 10:51:38 -0400

    yeah so i think for this pattern, you want to recognize when you divide things up into subarrays like this, how can you store the solution to the previous subarray, or store what you might need for the next subarray? So here it'd be storing both the max and the min is the key. you tried to do it with a FSM, and you were honestly kind of on the right path, but there was just a much better way to store the same information. you knew this, but you should try figuring out THE key instead of brute forcing a bad algorithm i think. i don't think you're learning when you do that.

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -7,39 +7,36 @@ class Solution:
 
         # 3 var: solution, cur_prod, last_prod
         # did not account for 0. i guess if it's zero then you just compare with the solution and if it's bigger at that the zero then you just make it the solution.
-        if len(nums) == 1:
-            return nums[0]
-        solution = nums[0]
-        temp = 0
-        even = True
-        cur_prod = nums[0]
+        # if len(nums) == 1:
+        #     return nums[0]
+        # solution = max(nums)
+        # temp = 0
+        # even = True
+        # cur_prod = nums[0]
 
-        for i in range(len(nums)):
-            print(i, ". ", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution),
-                  "even: " + str(even))
-            if i == 0:
-                if nums[i] < 0:
-                    even = False
-                continue
-            cur_prod *= nums[i]
-            if nums[i] == 0:
-                even = True
-            elif nums[i] < 0:
-                if not even:
-                    temp = cur_prod
-                    cur_prod = nums[i]
-                    even = True
-                else:
-                    cur_prod *= temp
-                    temp = 0
-                    even = False
+        # for i in range(len(nums)):
+        #     print(i, ". ", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution), "even: " + str(even))
+        #     if i == 0:
+        #         if nums[i] < 0:
+        #             even = False
+        #         continue
+        #     cur_prod *= nums[i]
+        #     if nums[i] == 0:
+        #         even = True
+        #     elif nums[i] < 0:
+        #         if not even:
+        #             temp = cur_prod
+        #             cur_prod = nums[i]
+        #             even = True
+        #         else:
+        #             cur_prod *= temp
+        #             temp = 0
+        #             even = False
 
-            solution = max(solution, cur_prod)
-            print(i, ". ", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution),
-                  "even: " + str(even))
-        print("last--", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution),
-              "even: " + str(even))
-        return max(solution, temp, cur_prod)
+        #     solution = max(solution, cur_prod)
+        #     print(i, ". ", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution), "even: " + str(even))
+        # print("last--", "cur_prod: " + str(cur_prod), "temp: " + str(temp), "solution: " + str(solution), "even: " + str(even))
+        # return solution
 
         # brute force: check every subarray
         # def prod(listo):
@@ -55,3 +52,20 @@ class Solution:
         #             maxo = max(maxo, prod(nums[i : j]))
         # return maxo
 
+        # okay i watched the neetcode vid.
+        # basically you want to store the min/max of each subarray so that way when you iterate across, you can multiply by the min/max an djust choose as your means of accounting for the negatives
+        # for the zeroes, you'd just reset the min/max.
+
+        mino = 1
+        maxo = 1
+        solution = max(nums)
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                maxo = 1
+                mino = 1
+            temp = maxo
+            maxo = max(nums[i], mino * nums[i], maxo * nums[i])
+            mino = min(nums[i], mino * nums[i], temp * nums[i])
+            solution = max(maxo, solution)
+
+        return solution
