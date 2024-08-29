730401a14bbbf930890328cee2de774138612fc9
Jerry Gu <ncq9fn@virginia.edu>
2024-08-29 10:57:14 -0400

    did some paint work and i think i have something to submit. if this doesn't work then i think we just neetcode that jit.

--- 98. Validate Binary Search Tree.py
+++ 98. Validate Binary Search Tree.py
@@ -9,11 +9,16 @@ class Solution:
         # is there a way where the greedy also breaks?
         # i dont hink so
         # wiat i'm dumb you can do it if the right brancha dn left branch have in between vals
-        # track the minimum value you've run into as you DFS through the tree i think
+        # track 15the minimum value you've run into as you DFS through the tree i think
         # need to figure out how to track it as we backtrack?
         # well if we start from the root then it works?? if i write the recursion properly...
+
+        # in-order traversal: track the last left move, and the last right move, then you need to see the ranges from the last left and the last right. so for root root which has no last left or right, if you go left, then your range is ::root.val, and lastleft becomes root.val. if you go right, then the range is root.val:: and your last right becomes root.val.
+        # so if go left, then go right, your value becomes cur.val::root.val, or making cur.val last right, lastright::lastleft. when i say lastright or lastleft, that refers to the left or right TURN, not the left or right VALUE.
         if not root:
             return False
+
+
 
 
 
