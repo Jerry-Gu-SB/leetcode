e51ea6d6b1c09f2ad773961ff10df509e44a5b43
Jerry Gu <ncq9fn@virginia.edu>
2024-10-12 21:32:58 -0400

    made some progress, my test cases are all working great except this one for some reason.

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,4 +1,10 @@
 class Solution:
+    # test cases
+    # [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]]
+    # [[1, 2, 3, 4]]
+    # [[1], [2], [3], [4]]
+    # [[1, 2, 3, 4], [5, 6, 7, 8]]
+    # [[1, 2], [3, 4]]
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         # bottom, top, left, right
         # use those varaibles to track which direction you need to go to.
@@ -6,7 +12,6 @@ class Solution:
 
         sol = []
         total = len(matrix) * len(matrix[0])
-
         STATE = "TOP"
 
         top = 0
@@ -14,29 +19,34 @@ class Solution:
         left = 0
         right = len(matrix[0])
 
-        while len(sol) <= total:
+        while len(sol) < total:
             if STATE == "TOP":
                 for i in range(left, right):
                     sol.append(matrix[top][i])
-                top += 1
+                if top < bottom:
+                    top += 1
                 STATE = "RIGHT"
 
             elif STATE == "RIGHT":
                 for i in range(top, bottom):
                     sol.append(matrix[i][right - 1])
-                right -= 1
+                if right > left:
+                    right -= 1
                 STATE = "BOT"
 
             elif STATE == "BOT":
-                for i in range(left, right, -1):
+                for i in reversed(range(left, right)):
                     sol.append(matrix[bottom - 1][i])
-                bottom -= 1
+                if bottom > top:
+                    bottom -= 1
                 STATE = "LEFT"
 
             elif STATE == "LEFT":
-                for i in range(bottom, top):
+                print("bottom:", bottom, "top:", top, "left:", left, "right:", right)
+                for i in reversed(range(bottom, top)):
                     sol.append(matrix[i][left])
-                left += 1
+                if right < left:
+                    left += 1
                 STATE = "TOP"
 
             print(sol)
