24b28945b2886df6c137e90e95cabb32ee3abf98
Jerry Gu <ncq9fn@virginia.edu>
2024-10-13 20:42:44 -0400

    solved it, but i actually didn't need to have the state machine in here lol

--- 54. Spiral Matrix.py
+++ 54. Spiral Matrix.py
@@ -1,10 +1,4 @@
 class Solution:
-    # test cases
-    # [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20], [21, 22, 23, 24]]
-    # [[1, 2, 3, 4]]
-    # [[1], [2], [3], [4]]
-    # [[1, 2, 3, 4], [5, 6, 7, 8]]
-    # [[1, 2], [3, 4]]
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         # bottom, top, left, right
         # use those varaibles to track which direction you need to go to.
@@ -23,31 +17,25 @@ class Solution:
             if STATE == "TOP":
                 for i in range(left, right):
                     sol.append(matrix[top][i])
-                if top < bottom:
-                    top += 1
+                top += 1
                 STATE = "RIGHT"
 
             elif STATE == "RIGHT":
                 for i in range(top, bottom):
                     sol.append(matrix[i][right - 1])
-                if right > left:
-                    right -= 1
+                right -= 1
                 STATE = "BOT"
 
             elif STATE == "BOT":
                 for i in reversed(range(left, right)):
                     sol.append(matrix[bottom - 1][i])
-                if bottom > top:
-                    bottom -= 1
+                bottom -= 1
                 STATE = "LEFT"
 
             elif STATE == "LEFT":
-                print("bottom:", bottom, "top:", top, "left:", left, "right:", right)
-                for i in reversed(range(bottom, top)):
+                for i in reversed(range(top, bottom)):
                     sol.append(matrix[i][left])
-                if right < left:
-                    left += 1
+                left += 1
                 STATE = "TOP"
 
-            print(sol)
         return sol
